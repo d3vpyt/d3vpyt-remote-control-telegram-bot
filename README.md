854aa79614e86001ec8e5c6e4b79c4a5f72d552d
d3vpyt <d3vpyt@gmail.com>
2021-03-09 01:06:41 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+### Telegram bot for computer control
+Bot to control sound, reboot and shutdown
+### ENV  
+In the environment variables, you need to set the bot API token  
+`BOT_TOKEN`="Telegram token"
+### SLASH COMMANDS  
+`/start` - Command for displaying control buttons
+### RUN  
+ - Need Python 3.8
+ - python run main.py
+ - Telegram
+### USE  
+ -  Run main.py
+ -  Click on the button you need
+
---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils import executor
+from aiogram.utils.callback_data import CallbackData
+
+bot = Bot(os.getenv('BOT_TOKEN'))
+dp = Dispatcher(bot)
+keyboards_query = CallbackData('keyboard', 'action', 'value')
+
+
+def create_menu_keyboard():
+    return InlineKeyboardMarkup(row_width=2).add(
+        InlineKeyboardButton('🌗 Перезагрузка',
+                             callback_data=keyboards_query.new(action='remote_control', value='reboot')),
+        InlineKeyboardButton('➕ Грокмкость', callback_data=keyboards_query.new(action='remote_control',
+                                                                               value='amixer -D pulse sset Master 10%+')),
+        InlineKeyboardButton('🌙 Выключение',
+                             callback_data=keyboards_query.new(action='remote_control',
+                                                               value='shutdown -P 0.30')),
+        InlineKeyboardButton('➖ Грокмкость', callback_data=keyboards_query.new(action='remote_control',
+                                                                               value='amixer -D pulse sset Master 10%-'))
+    )
+
+
+@dp.message_handler(commands='start')
+async def select_report(message: types.Message):
+    await bot.send_message(message.chat.id, 'Выберите действие', reply_markup=create_menu_keyboard())
+
+
+@dp.callback_query_handler(keyboards_query.filter(action='remote_control'))
+async def send_repo(query: types.CallbackQuery, callback_data: dict):
+    os.system(callback_data['value'])
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
